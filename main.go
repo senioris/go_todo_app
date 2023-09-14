a6d4fb4568e0ba75bbae9b8f7060c1766333d443
Senioris <3rd.hierarchy@gmail.com>
2023-09-14 21:53:15 +0900

    Section 6

--- main.go
+++ main.go
@@ -5,19 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
-	"os"
 
 	"github.com/senioris/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-
 	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 	}
@@ -35,25 +27,7 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
